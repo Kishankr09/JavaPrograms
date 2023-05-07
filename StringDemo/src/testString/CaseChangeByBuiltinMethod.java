721fe3fa9fae3fc4ac4bfce5dbf308d4f31ceac8
Kishan Kumar <68832175+Kishankr09@users.noreply.github.com>
2023-05-07 18:25:38 +0530

    Add files via upload

---
+++ StringDemo/src/testString/CaseChangeByBuiltinMethod.java
@@ -0,0 +1,30 @@
+package testString;
+
+import java.util.Scanner;
+
+public class CaseChangeByBuiltinMethod {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Any Number ");
+		String str1 = s.nextLine();
+		StringBuffer newStr=new StringBuffer(str1);    
+        
+        for(int i = 0; i < str1.length(); i++) {    
+                
+           
+            if(Character.isLowerCase(str1.charAt(i))) {    
+                   
+                newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));    
+            }    
+              
+            else if(Character.isUpperCase(str1.charAt(i))) {    
+                  
+                newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));    
+            }    
+        }    
+        System.out.println("String after case conversion : \n" + newStr);  
+
+	}
+
+}
