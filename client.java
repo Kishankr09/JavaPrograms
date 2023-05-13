4982c62e7f774d6a2c852a2b2d056de755a025db
Kishan Kumar <68832175+Kishankr09@users.noreply.github.com>
2023-05-14 00:02:35 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,15 @@
+interface one {
+	public void methodOne();
+}
+
+interface Two{
+	public void methodTwo();
+}
+class Three implements one, Two{
+	public void methodOne() {
+		
+	}
+	public void methodTwo() {
+		
+	}
+}
